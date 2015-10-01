1f079969dc228dc366f8b09e8308d5765ee7af6a
NazarZayats <nazarz@ex.ua>
2015-10-01 18:24:55 +0300

    Create bash_script.sh
    Rename existing folder

---
+++ bash_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+read -p "Enter path: " value1
+cd $value1
+if [ $? == 0 ]
+	then
+		read -p "Enter dir name: " value2
+		if [ -d $value2 ]
+			then
+				read -p "Enter NEW name: " value3
+				mv $value2 $value3
+				if [ -d $value3 ]
+					then 
+						echo 'Complete'
+				  else
+					  echo 'Rename failed'
+		  	fi
+			else
+				echo 'Folder doese not exist'
+		fi
+		else 
+			echo 'Bad path!!'
+fi
