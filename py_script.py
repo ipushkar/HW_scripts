1e3a430d7f0b0f51af21f8bc7db197154c9dbf41
NazarZayats <nazarz@ex.ua>
2015-10-01 18:25:38 +0300

    Create py_script.py
    Rename existing folder using  Python

---
+++ py_script.py
@@ -0,0 +1,9 @@
+#
+#
+import os
+value1=raw_input('Enter path: ')
+os.chdir(value1)
+value2 =raw_input('Enter dir name: ')
+value3 =raw_input('Enter NEW name: ')
+os.rename(value2,value3)
+print 'Complete'
