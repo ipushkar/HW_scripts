eefeef7794e6f7e49f6d66d9ab74697a7c723aa7
NazarZayats <nazarz@ex.ua>
2015-10-07 19:40:54 +0300

    Update bash_check.sh

--- bash_check.sh
+++ bash_check.sh
@@ -5,12 +5,12 @@ cd $value1
 if [ $? == 0 ]
 	then
 		read -p "Enter old dir name: " value2
-		cd $value2  
-		if [ $? == 0 ] 
+#		cd $value2 
+		if [ -d $value2 ]
 		  then 
 		    echo "Folder doese not exist"
 				read -p "Enter NEW name: " value3
-					if [ -d $value3 ]
+					
 					then 
 						echo 'Renam succes'
 				  else
