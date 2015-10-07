229c74798b756abd4bb683c00ca038f69149d945
NazarZayats <nazarz@ex.ua>
2015-10-07 20:03:25 +0300

    Update bash_check.sh

--- bash_check.sh
+++ bash_check.sh
@@ -4,21 +4,19 @@ read -p "Enter path: " value1
 cd $value1
 if [ $? == 0 ]
 	then
-		read -p "Enter old dir name: " value2
-#		cd $value2 
-		if [ -d $value2 ]
-		  then 
-		    echo "Folder doese not exist"
-				read -p "Enter NEW name: " value3
-					
-					then 
-						echo 'Renam succes'
-				  else
+		read -p "Enter old folder name: " value2
+			if [ -d $value2 ]
+				then 
+				echo "Folder exists, rename failed"
+				read -p "Enter NEW folder name: " value3
+					if [ -d $value3 ]
+						then 
+						echo 'Rename succes'
+					 else
 					  echo 'Rename failed'
-		  	fi
-			else
-				echo 'Folder doese not exist'
-		fi
-		else 
-			echo 'Bad path!!'
+		  			fi
+			
+			fi
+	else 
+		echo 'Bad path!!'
 fi
