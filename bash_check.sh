08986c25dba6130ac11647dde2fce0992b02310c
NazarZayats <nazarz@ex.ua>
2015-10-07 17:49:41 +0300

    Create bash_check.sh

---
+++ bash_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+read -p "Enter path: " value1
+cd $value1
+if [ $? == 0 ]
+	then
+		read -p "Enter old dir name: " value2
+		cd $value2  
+		if [ $? == 0 ] 
+		  then 
+		    echo "Folder doese not exist"
+				read -p "Enter NEW name: " value3
+					if [ -d $value3 ]
+					then 
+						echo 'Renam succes'
+				  else
+					  echo 'Rename failed'
+		  	fi
+			else
+				echo 'Folder doese not exist'
+		fi
+		else 
+			echo 'Bad path!!'
+fi
