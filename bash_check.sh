ca51365846c1b4c553ec547f3d67030c15cb2a1d
NazarZayats <nazarz@ex.ua>
2015-10-08 19:05:15 +0300

    Update bash_check.sh

--- bash_check.sh
+++ bash_check.sh
@@ -1,47 +1,24 @@
 #!/bin/bash
 #
-#read -p "Enter path: " value1
-#cd $value1
-#if [ $? == 0 ]
-#	then
-#		read -p "Enter old folder name: " value2
-#			if [ -d $value2 ]
-#				then 
-#				echo "Folder exists, rename failed"
-#				else
-#					echo "<< Old folder doesn't exist >>"
-#					read -p "Enter NEW folder name: " value3
-#						if [ -d $value3 ]
-#							then 
-#							echo '<< Folder was renamed >>'
-#						 else
-#						echo "<< Folder wasn't renamed >>"
-#		  				fi
-#			
-#			fi
-#	else 
-#		echo '<< Bad path!! >>'
-#fi
-##################################################################################################
-# ver. 2 
-#read -p "Enter path: " value1
+read -p "Enter path: " value1
 cd $value1
 if [ $? == 0 ]
 	then
-		if [ -d $value2 ]
-			then 
-			echo "Folder exists, rename failed"
-		else
-			echo "<< Old folder doesn't exist >>"
-			if [ -d $value3 ]
+		read -p "Enter old folder name: " value2
+			if [ -d $value2 ]
 				then 
-					echo '<< Folder was renamed >>'
-			else
-				echo "<< Folder wasn't renamed >>"
-		  	fi
+				echo "Folder exists, rename failed"
+				else
+					echo "<< Old folder doesn't exist >>"
+					read -p "Enter NEW folder name: " value3
+						if [ -d $value3 ]
+							then 
+							echo '<< Folder was renamed >>'
+						 else
+						echo "<< Folder wasn't renamed >>"
+		  				fi
 			
-		fi
+			fi
 	else 
 		echo '<< Bad path!! >>'
 fi
-
