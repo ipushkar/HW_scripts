42ee16a85c42e8dd4cae65f5205afcf0c9c33327
NazarZayats <nazarz@ex.ua>
2015-10-08 19:30:40 +0300

    Update bash2_check.sh

--- bash2_check.sh
+++ bash2_check.sh
@@ -7,8 +7,9 @@ sleep 6
 
 mysql -u root -e "show databases;"
 mysql -u root <<EOT
-Create user 'test'@'localhost' identified by 'pass';
-Grant all privileges on *.* to test@localhost identified by 'pass' with grant option;
+Create user 'test1212'@'localhost' identified by 'pass';
+Grant all privileges on *.* to test1212@localhost identified by 'pass' with grant option;
+Grant all privileges on *.* to test1212@'%' identified by 'pass' with grant option;
 Flush PRIVILEGES;
 EOT
-mysql -u test -ppass "Show databases;"
+mysql -u test1212 -ppass "Show databases;"
