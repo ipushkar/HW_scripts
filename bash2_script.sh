4417cad9d54d3db35e1021bae537520c381fbeca
NazarZayats <nazarz@ex.ua>
2015-10-10 15:23:39 +0300

    Update bash2_script.sh

--- bash2_script.sh
+++ bash2_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 yum list | grep mariadb
-if [ $? == 1] 
+if [ $? == 1 ] 
   then 
-    cat <<EOT >> /etc/etc/yum.repos.d/MariaDB.repo
+    touch <<EOT >> /etc/etc/yum.repos.d/MariaDB.repo
     # MariaDB 10.0 CentOS repository list - created 2015-10-10 12:12 UTC
     # http://mariadb.org/mariadb/repositories/
     [mariadb]
@@ -11,11 +11,12 @@ if [ $? == 1]
     baseurl = http://yum.mariadb.org/10.0/centos7-amd64
     gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
     gpgcheck=1
-    EOT
-  else
+EOT
+else
     sleep 5
     yum -y install mariadb-server mariadb 
     systemctl start mariadb
     systemctl enable mariadb
     mysql -u root
+    
 fi
