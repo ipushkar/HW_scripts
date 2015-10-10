53412c65981eca1084169bef91b2a24e90dd782a
NazarZayats <nazarz@ex.ua>
2015-10-10 15:26:45 +0300

    Update bash2_script.sh

--- bash2_script.sh
+++ bash2_script.sh
@@ -3,7 +3,7 @@
 yum list | grep mariadb
 if [ $? == 1 ] 
   then 
-    touch <<EOT >> /etc/yum.repos.d/MariaDB.repo
+    cat <<EOT >> /etc/yum.repos.d/MariaDB.repo
     # MariaDB 10.0 CentOS repository list - created 2015-10-10 12:12 UTC
     # http://mariadb.org/mariadb/repositories/
     [mariadb]
