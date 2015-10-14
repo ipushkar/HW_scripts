3fbbe6690bb9635931a239304d01788ebe923c8c
NazarZayats <nazarz@ex.ua>
2015-10-14 13:28:20 +0300

    Update bash2_check.sh

--- bash2_check.sh
+++ bash2_check.sh
@@ -7,7 +7,7 @@ sleep 6
 
 # mysql -u root -e "show databases;"
 mysql -u root <<EOT
-Create user 'test1212'@'localhost' identified by 'pass';
+Create user test1212@localhost identified by 'pass';
 Grant all privileges on *.* to test1212@localhost identified by 'pass' with grant option;
 #Grant all privileges on *.* to test1212@'%' identified by 'pass' with grant option;
 #Create user 'test12'@'localhost' identified by 'pass';
@@ -23,9 +23,9 @@ sleep 5
 mysql -u test1212 -ppass -e "use test; create table t1 ( t1 varchar(20), t2 varchar(20) );"
 mysql -u test1212 -ppass <<EOT
 INSERT ALL
-  INTO mytable (t1, t2) VALUES (data1, data2)
-  INTO mytable (t1, t2) VALUES (data3, data4)
-  INTO mytable (t1, t2) VALUES (data5, data6);
+  INTO t1 (t1, t2) VALUES (data1, data2)
+  INTO t1 (t1, t2) VALUES (data3, data4)
+  INTO t1 (t1, t2) VALUES (data5, data6);
 EOT
 mysql -u test12 -ppass -e "use test; select * from t1;"
 sleep 5
