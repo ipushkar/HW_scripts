813c1c9de1a2acd6bf35ecffe54d99509ed6f762
NazarZayats <nazarz@ex.ua>
2015-10-14 12:58:19 +0300

    Update bash2_check.sh

--- bash2_check.sh
+++ bash2_check.sh
@@ -11,17 +11,18 @@ Create user 'test1212'@'localhost' identified by 'pass';
 Grant all privileges on *.* to test1212@localhost identified by 'pass' with grant option;
 #Grant all privileges on *.* to test1212@'%' identified by 'pass' with grant option;
 Create user 'test12'@'localhost' identified by 'pass';
-Grant Select on *.* to test12@localhost identified by 'pass' with grant option;
-#Grant Select on *.* to test12@'%' identified by 'pass' with grant option;
+Grant Select on *.* to test12@localhost identified by 'pass';
+#Grant Select on *.* to test12@'%' identified by 'pass';
 Flush PRIVILEGES;
 EOT
-mysql -u test12 -ppass -e "show databases;"
-mysql -u test1212 -ppass -e "show grants for test1212;"
+#mysql -u test12 -ppass -e "show databases;"
+mysql -u test1212 -ppass -e "show grants for test1212@localhost;"
 sleep 5
-mysql -u test1212 -ppass -e "show grants for test12;"
+mysql -u test1212 -ppass -e "show grants for test12@localhost;"
 sleep 3 
 mysql -u test1212 -ppass -e "use test; create table t1 ( t1 varchar(20), t2 varchar(20) );"
 mysql -u test12 -ppass -e "use test; drop table t1;"
+mysql -u test12 -ppass -e "show databases;"
 #
 #
 
