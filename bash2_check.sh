2780d261cb2e8712df6d42423775e6f36036028b
NazarZayats <nazarz@ex.ua>
2015-10-14 12:43:15 +0300

    Update bash2_check.sh

--- bash2_check.sh
+++ bash2_check.sh
@@ -7,11 +7,11 @@ sleep 6
 
 # mysql -u root -e "show databases;"
 mysql -u root <<EOT
-Create user 'test1212'@'localhost' identified by 'pass';
-Grant all privileges on *.* to test1212@localhost identified by 'pass' with grant option;
+Create user 'test1212'@'%' identified by 'pass';
+#Grant all privileges on *.* to test1212@localhost identified by 'pass' with grant option;
 Grant all privileges on *.* to test1212@'%' identified by 'pass' with grant option;
-Create user 'test12'@'localhost' identified by 'pass';
-Grant Select on *.* to test12@localhost identified by 'pass' with grant option;
+Create user 'test12'@'%' identified by 'pass';
+#Grant Select on *.* to test12@localhost identified by 'pass' with grant option;
 Grant Select on *.* to test12@'%' identified by 'pass' with grant option;
 Flush PRIVILEGES;
 EOT
@@ -20,7 +20,8 @@ mysql -u test1212 -ppass -e "show grants for test1212;"
 sleep 5
 mysql -u test1212 -ppass -e "show grants for test12;"
 sleep 3 
-mysql -u test12 -ppass -e "use test; create table t1 ( t1 varchar(20), t2 varchar(20) );"
+mysql -u test1212 -ppass -e "use test; create table t1 ( t1 varchar(20), t2 varchar(20) );"
+mysql -u test12 -ppass -e "use test; drop table t1;"
 #
 #
 
